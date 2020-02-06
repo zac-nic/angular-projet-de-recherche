931da7e986eaffefb30b318851329f5a4fc8959f
zac-nic <zacnicbigz@gmail.com>
2020-02-05 21:04:25 -0500

    4 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,11 +11,11 @@ export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert('Le livre a ete partager!');
   }
 
   onNotify() {
-    window.alert('You will be notified when the product goes on sale');
+    window.alert('Vous serez averti lorsque le livre sera en rabais!');
   }
 }
 
